1313e87130b00354ad06d16e4763bbd321be7424
baltasarcarrasco <88855315+baltasarcarrasco@users.noreply.github.com>
2024-07-03 20:23:01 +0200

    Evaluation added

--- cloud_deploy/sagemaker_deploy/deploy.py
+++ cloud_deploy/sagemaker_deploy/deploy.py
@@ -14,24 +14,30 @@ role = get_execution_role()
 bucket = "s3://yolo-deployment"
 model_data = f"{bucket}/model.tar.gz"
 
-model_name = 'yolov8l.pt'
+model_name = "yolov8l.pt"
 
-model = PyTorchModel(entry_point='inference.py',
-                     model_data=model_data,
-                     framework_version='1.12',
-                     py_version='py38',
-                     role=role,
-                     env={'TS_MAX_RESPONSE_SIZE':'20000000', 'YOLOV8_MODEL': model_name},
-                     sagemaker_session=sess)
+model = PyTorchModel(
+    entry_point="inference.py",
+    model_data=model_data,
+    framework_version="1.12",
+    py_version="py38",
+    role=role,
+    env={"TS_MAX_RESPONSE_SIZE": "20000000", "YOLOV8_MODEL": model_name},
+    sagemaker_session=sess,
+)
 
 print("Model created")
 
-INSTANCE_TYPE = 'ml.m5.4xlarge'
-ENDPOINT_NAME = 'yolov8-pytorch-' + str(datetime.utcnow().strftime('%Y-%m-%d-%H-%M-%S-%f'))
+INSTANCE_TYPE = "ml.m5.4xlarge"
+ENDPOINT_NAME = "yolov8-pytorch-" + str(
+    datetime.utcnow().strftime("%Y-%m-%d-%H-%M-%S-%f")
+)
 
-predictor = model.deploy(initial_instance_count=1,
-                         instance_type=INSTANCE_TYPE,
-                         deserializer=JSONDeserializer(),
-                         endpoint_name=ENDPOINT_NAME)
+predictor = model.deploy(
+    initial_instance_count=1,
+    instance_type=INSTANCE_TYPE,
+    deserializer=JSONDeserializer(),
+    endpoint_name=ENDPOINT_NAME,
+)
 
 print("Model deployed")
