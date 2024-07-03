bf984412499df0fcd880e75e06c88a3add319098
baltasarcarrasco <88855315+baltasarcarrasco@users.noreply.github.com>
2024-07-03 20:49:20 +0200

    Evaluation data added

---
+++ evaluation/README.dataset.txt
@@ -0,0 +1,6 @@
+# deep > 2024-06-30 1:09pm
+https://universe.roboflow.com/ie-l0fg6/deep-ncu0q
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ evaluation/evaluate.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+from roboflow import Roboflow
+
+# rf = Roboflow(api_key="5Gtyuy6iNi3PDjaEYyuK")
+# project = rf.workspace("ie-l0fg6").project("deep-ncu0q")
+# version = project.version(1)
+# dataset = version.download("yolov8")
+
+# # Load a model
+model = YOLO("yolov8n.pt")
+
+# Customize validation settings
+validation_results = model.val(data="data.yaml")
